64cb8696dba28ad8c01c39e362d3f2b787659c8e
Mikio Nakata <21nakatam@gmail.com>
2022-10-15 17:41:17 +0900

    Initial Commit

---
+++ Sales Tracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Sales Tracker
+//
+//  Created by Mikio Nakata on 2022/10/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
