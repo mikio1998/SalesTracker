a607d163c98b00699f11c205c120f183bf8801c2
Mikio Nakata <21nakatam@gmail.com>
2022-11-05 18:43:09 +0900

    🔥 Removed comments.

--- Sales Tracker/Presentation/Camera/Presenter/CameraViewController.swift
+++ Sales Tracker/Presentation/Camera/Presenter/CameraViewController.swift
@@ -14,11 +14,8 @@ protocol CameraViewPresenterLike: AnyObject {
 final class CameraViewController: UIViewController {
     
     private let viewContainer: CameraViewLike
-//    private var videoLayer: AVCaptureVideoPreviewLayer
     private var model: CameraModel
-    var videoLayer = AVCaptureVideoPreviewLayer()
 
-//    init(viewContainer: CameraViewLike = CameraView()) {
     init(viewContainer: CameraViewLike = CameraView(), model: CameraModel = CameraModelImpl()) {
         self.viewContainer = viewContainer
         self.model = model
@@ -34,8 +31,6 @@ final class CameraViewController: UIViewController {
     }
     
 //    override func viewWillAppear(_ animated: Bool) {
-////        super.viewWillAppear(true)
-//        configureVideoSession()
 //    }
 //    override func viewWillDisappear(_ animated: Bool) {
 //    }
@@ -53,63 +48,13 @@ final class CameraViewController: UIViewController {
             }
         }
     }
-    
-//    func configureVideoSession() {
-//        // CALayer of video stream from device camera.
-////        videoLayer = AVCaptureVideoPreviewLayer()
-//
-//        // Session input and output.
-//        // - Session gets raw materials (data) from the input (the camera capture)
-//        // - Session produces output depending on how we ask it to (qr code read, etc.).
-//        let session = AVCaptureSession()
-//
-//        // MARK: Input
-//        // Returns video as capture device, and make it the session input device.
-//        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
-//        do {
-//            let input = try AVCaptureDeviceInput(device: captureDevice)
-//            session.addInput(input)
-//        } catch {
-//            print("Failure to register capture device.")
-//        }
-//
-//
-//
-//        // MARK: Output
-//        let output = AVCaptureMetadataOutput()
-//        session.addOutput(output)
-//        // Process the output on the main thread (best performance).
-//        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-//        // Define type of output we want (qr code, ean13 barcode).
-//        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr, AVMetadataObject.ObjectType.ean13]
-//
-////        output.metadataObjectTypes = output.availableMetadataObjectTypes
-////        output.metadataObjectTypes = []
-//
-////        session.addOutput(output)
-//
-//        // CALayer of video stream from device camera.
-//        videoLayer = AVCaptureVideoPreviewLayer(session: session)
-//
-//
-//        // Set bounds, and add as Sublayer to View (delegate method).
-//        viewContainer.showVideo(layer: videoLayer)
-//
-//        // Start the session.
-//        session.startRunning()
-//    }
-    
-    
-    
 }
 
 extension CameraViewController: AVCaptureMetadataOutputObjectsDelegate {
     func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
         print("output")
     }
-
 }
 
 extension CameraViewController: CameraViewPresenterLike {
-    
 }
--- Sales Tracker/Presentation/Camera/View/CameraView.swift
+++ Sales Tracker/Presentation/Camera/View/CameraView.swift
@@ -11,7 +11,6 @@ import AVFoundation
 protocol CameraViewLike: ViewContainer {
     var presenterLike: CameraViewPresenterLike? { get set }
     func showVideo(layer: AVCaptureVideoPreviewLayer)
-    func showVid()
 }
 
 final class CameraView: XibView {
@@ -29,60 +28,17 @@ final class CameraView: XibView {
     }
 
 }
+
 extension CameraView: CameraViewLike {
     func showVideo(layer: AVCaptureVideoPreviewLayer) {
         print("show")
-        
         self.videoLayer = layer
         DispatchQueue.main.async {
             self.videoLayer.frame = self.layer.bounds
             self.layer.addSublayer(self.videoLayer)
         }
-        
-        
     }
-    
-    func showVid() {
-//        // CALayer of video stream from device camera.
-////        videoLayer = AVCaptureVideoPreviewLayer()
-//
-//        // Session input and output.
-//        // - Session gets raw materials (data) from the input (the camera capture)
-//        // - Session produces output depending on how we ask it to (qr code read, etc.).
-//        let session = AVCaptureSession()
-//
-//        // MARK: Input
-//        // Returns video as capture device, and make it the session input device.
-//        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
-//        do {
-//            let input = try AVCaptureDeviceInput(device: captureDevice)
-//            session.addInput(input)
-//        } catch {
-//            print("Failure to register capture device.")
-//        }
-//
-//        // MARK: Output
-//        let output = AVCaptureMetadataOutput()
-//        // Process the output on the main thread (best performance).
-//        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
-//        // Define type of output we want (qr code, ean13 barcode).
-////        output.metadataObjectTypes = [AVMetadataObject.ObjectType.qr, AVMetadataObject.ObjectType.ean13]
-//        session.addOutput(output)
-//
-//        // CALayer of video stream from device camera.
-//        videoLayer = AVCaptureVideoPreviewLayer(session: session)
-//
-//        DispatchQueue.main.async {
-//            self.videoLayer.frame = self.layer.bounds
-//            self.layer.addSublayer(self.videoLayer)
-//        }
-//
-//        // Start the session.
-//        session.startRunning()
-    }
-    
-    
-    
 }
+
 extension CameraView: AVCaptureMetadataOutputObjectsDelegate {}
 
