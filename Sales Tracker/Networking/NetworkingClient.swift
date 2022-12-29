5d6f78d05f8bbdd7e0303364ba3250fe7393512e
Mikio Nakata <21nakatam@gmail.com>
2022-12-29 13:42:04 +0900

    🚧 WIP Api Networking Client

---
+++ Sales Tracker/Networking/NetworkingClient.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingClient.swift
+//  Sales Tracker
+//
+//  Created by Mikio Nakata on 2022/12/29.
+//
+
+import Foundation
+import Alamofire
+
+class NetworkingClient {
+
+    func execute(_ url: URL) {
+        }
+    }
+
+}
