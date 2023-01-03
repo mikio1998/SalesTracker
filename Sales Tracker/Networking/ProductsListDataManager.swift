341915063d9ea91851b334ddb1a2688024ef79d1
Mikio Nakata <21nakatam@gmail.com>
2023-01-03 14:09:13 +0900

    🎨 Created nonoptional reload in ProductsListDataManager, if reload is set as false but no data is cached.

--- Sales Tracker/Networking/ProductsListDataManager.swift
+++ Sales Tracker/Networking/ProductsListDataManager.swift
@@ -17,7 +17,11 @@ final class ProductsListDataManager {
     // Fetch cache, or perform request.
     static func loadList(reload: Bool = false, completion: @escaping (Result<Prods, Error>) -> Void) {
         if Self.shared.data != nil && reload == false {
-            guard let data = Self.shared.data else { return }
+            guard let data = Self.shared.data else {
+                // If reload is false but no data, force a reload.
+                loadList(reload: true, completion: completion)
+                return
+            }
             completion(.success(data))
         } else {
             ServiceLayer.request(router: Router.getProductsList(vendor: nil), completion: completion)
