9e7762ad582063dea1aa5ef2278411511078a87f
Mikio Nakata <21nakatam@gmail.com>
2023-01-03 13:33:25 +0900

    ⚡Added ProductsListDataManager to keep cache of Products list.

--- Sales Tracker/Networking/ProductsListDataManager.swift
+++ Sales Tracker/Networking/ProductsListDataManager.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+final class ProductsListDataManager {
+    static let shared = ProductsListDataManager()
+
+    private var data: Prods? // Cache
+
+    private init() {}
+
+    // Fetch cache, or perform request.
+    static func loadList(reload: Bool = false, completion: @escaping (Result<Prods, Error>) -> Void) {
+        if Self.shared.data != nil && reload == false {
+            guard let data = Self.shared.data else { return }
+            completion(.success(data))
+        } else {
+            ServiceLayer.request(router: Router.getProductsList(vendor: nil), completion: completion)
+        }
+    }
+}
