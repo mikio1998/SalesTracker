f01f8cc312d9d265a204051d8231d617fe186b39
Mikio Nakata <21nakatam@gmail.com>
2023-01-23 18:07:56 +0900

    Added basic methods to SettingsVM

--- Sales Tracker/Presentation/Settings/ViewModel/SettingsViewModel.swift
+++ Sales Tracker/Presentation/Settings/ViewModel/SettingsViewModel.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+
+class SettingsViewModel {
+    var presentAlert: (() -> Void)?
+
+    var toggleSVProgressHUD: (() -> Void)?
+
+    var alert: (title: String, msg: String)? {
+        didSet {
+            self.presentAlert?()
+        }
+    }
+
+    var isLoading: Bool = false {
+        didSet {
+            self.toggleSVProgressHUD?()
+        }
+    }
+
+    init() {
+
+    }
+
+    var settingsCellViewModels = [SettingsCellViewModel]() {
+//        didSet {
+//            if salesHistoryCellViewModels.isEmpty {
+//                self.showNoResults?(nil)
+//            } else {
+//                self.reloadTableView?()
+//            }
+//            self.isLoading = false
+//        }
+    }
+    
+    func getCellViewModel(at indexPath: IndexPath) -> SettingsCellViewModel {
+        return settingsCellViewModels[indexPath.row]
+    }
+
+    func didSelectRow(at indexPath: IndexPath) {
+
+    }
+}
